68e853edb48a38c0647889e9c64c1ed7f1d9f2ce
pgleeson <p.gleeson@gmail.com>
2025-07-01 17:34:51 -0700

    Improved sibernetic loading: shows boundary with -b

--- siberneticmodel.py
+++ siberneticmodel.py
@@ -7,18 +7,30 @@ last_mesh = None
 all_points = []
 all_point_types = []
 
-colours = {1.1: "lightblue", 2.1: "green", 2.2: "turquoise", 3: "#eeeeee"}
-colours = {1.1: "blue", 2.2: "turquoise"}
+boundary_points = []
+boundary_point_types = [3, 3.1]
+
+point_size = 5
+boundary_point_size = 3
+
+boundary_color = "#eeeeee"
 
 plotter = None
 
 offset_ = 50
 
+color_range = {1.1: "blue", 2.2: "turquoise"}
+
 
 def add_sibernetic_model(
-    pl, position_file="position_buffer.txt", swap_y_z=False, offset=50
+    pl,
+    position_file="position_buffer.txt",
+    swap_y_z=False,
+    offset=50,
+    include_boundary=False,
 ):
     global all_points, all_point_types, last_mesh, plotter, offset_
+
     offset_ = offset
     plotter = pl
 
@@ -29,8 +41,6 @@ def add_sibernetic_model(
     pcount = 0
     time_count = 0
     logStep = None
-
-    include_boundary = False
 
     for line in open(position_file):
         ws = line.split()
@@ -47,14 +57,28 @@ def add_sibernetic_model(
             logStep = int(ws[0])
 
         if len(ws) == 4:
-            type = float(ws[3])
+            type_ = float(ws[3])
 
-            if not (type == 3 and not include_boundary):
+            if type_ not in boundary_point_types:
                 if swap_y_z:
                     points.append([float(ws[1]), 1 * float(ws[0]), float(ws[2])])
                 else:
                     points.append([float(ws[0]), float(ws[1]), float(ws[2])])
-                types.append(type)
+
+                types.append(type_)
+
+            else:
+                if include_boundary:
+                    if swap_y_z:
+                        boundary_points.append(
+                            [float(ws[1]), 1 * float(ws[0]), float(ws[2])]
+                        )
+                    else:
+                        boundary_points.append(
+                            [float(ws[0]), float(ws[1]), float(ws[2])]
+                        )
+
+                    # types.append(type_)
 
         if logStep is not None:
             pcount += 1
@@ -89,6 +113,17 @@ def add_sibernetic_model(
         )
     )
 
+    if include_boundary:
+        bound_mesh = pv.PolyData(boundary_points)
+        bound_mesh.translate((offset_, -50, -100), inplace=True)
+
+        plotter.add_mesh(
+            bound_mesh,
+            render_points_as_spheres=True,
+            color=boundary_color,
+            point_size=boundary_point_size,
+        )
+
     print("Num of time points found: %i" % len(all_points))
 
     create_mesh(0)
@@ -96,7 +131,7 @@ def add_sibernetic_model(
     plotter.remove_scalar_bar("types")
 
     max_time = len(all_points) - 1
-    pl.add_slider_widget(create_mesh, rng=[0, max_time], value=30, title="Time point")
+    pl.add_slider_widget(create_mesh, rng=[0, max_time], value=0, title="Time point")
     pl.add_timer_event(max_steps=5, duration=2, callback=create_mesh)
 
 
@@ -113,6 +148,8 @@ def create_mesh(step):
     curr_points = all_points[index]
     curr_types = all_point_types[index]
 
+    print("Plotting %i points with %i types" % (len(curr_points), len(curr_types)))
+
     if last_mesh is None:
         last_mesh = pv.PolyData(curr_points)
         last_mesh["types"] = curr_types
@@ -123,15 +160,14 @@ def create_mesh(step):
         plotter.add_mesh(
             last_mesh,
             render_points_as_spheres=True,
-            cmap=[c for c in colours.values()],
-            point_size=3,
+            cmap=[c for c in color_range.values()],
+            point_size=point_size,
         )
     else:
         last_mesh.points = curr_points
         last_mesh.translate((offset_, -50, -100), inplace=True)
 
     plotter.render()
-
     time.sleep(0.1)
 
     return
@@ -141,12 +177,18 @@ if __name__ == "__main__":
     plotter = pv.Plotter()
 
     position_file = "position_buffer.txt"
+    include_boundary = False
+
+    if "-b" in sys.argv:
+        include_boundary = True
 
     if len(sys.argv) > 1 and os.path.isfile(sys.argv[1]):
         position_file = sys.argv[1]
 
-    add_sibernetic_model(plotter, swap_y_z=True)
-    plotter.set_background("lightgrey")
+    add_sibernetic_model(
+        plotter, position_file, swap_y_z=True, include_boundary=include_boundary
+    )
+    plotter.set_background("white")
     plotter.add_axes()
     # plotter.set_viewup([0, 0, 10])
 
