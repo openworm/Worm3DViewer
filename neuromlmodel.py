fe066e1fbad1e617f8ef7bcd179f565b9247d28b
Padraig Gleeson <p.gleeson@gmail.com>
2026-07-11 11:04:30 +0800

    More efficient nml viewer

--- neuromlmodel.py
+++ neuromlmodel.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pyvista as pv
 
 import sys
@@ -7,11 +8,7 @@ from pyneuroml.utils import extract_position_info
 from neuroml import Cell
 
 
-def add_neuroml_model(
-    plotter, filename="NeuroML2/c302_A_Full.net.nml", somas_only=False, factor=0.2
-):
-    """sphere = pv.Sphere(radius=5, center=(10, 10, 10))
-    plotter.add_mesh(sphere, color="red")"""
+def add_neuroml_model(plotter, filename, somas_only=False, factor=0.2):
 
     nml_doc = pynml.read_neuroml2_file(filename, include_includes=True)
 
@@ -25,8 +22,7 @@ def add_neuroml_model(
         pop_id_vs_radii,
     ) = extract_position_info(nml_doc, False)
 
-    while pop_id_vs_cell:
-        pop_id, cell = pop_id_vs_cell.popitem()
+    for pop_id, cell in pop_id_vs_cell.items():
         pos_pop = positions[pop_id]  # type: typing.Dict[typing.Any, typing.List[float]]
 
         print("Pop: %s has %i of component %s" % (pop_id, len(pos_pop), cell.id))
@@ -34,16 +30,24 @@ def add_neuroml_model(
         radius = pop_id_vs_radii[pop_id] if pop_id in pop_id_vs_radii else 0.5
         color = pop_id_vs_color[pop_id] if pop_id in pop_id_vs_color else "r"
 
-        if type(cell) is Cell:
+        # instance positions of this population, remapped/scaled to viewer axes
+        points = np.array(
+            [
+                [pos[0] * factor, pos[2] * factor, -1 * pos[1] * factor]
+                for pos in pos_pop.values()
+            ]
+        )
+        point_cloud = pv.PolyData(points)
+
+        if isinstance(cell, Cell):
             print("Loading a cell with %i segments" % len(cell.morphology.segments))
 
-            cell_meshes = pv.MultiBlock()
+            seg_meshes = []
 
             for seg in cell.morphology.segments:
                 p = cell.get_actual_proximal(seg.id)
                 d = seg.distal
                 width = (p.diameter + d.diameter) / 4
-                # print("Creating %s" % (seg))
                 if somas_only and seg.id != 0:
                     continue
 
@@ -52,8 +56,6 @@ def add_neuroml_model(
                         center=(p.x * factor, p.z * factor, -1 * p.y * factor),
                         radius=p.diameter * factor / 2,
                     )
-
-                    plotter.add_mesh(seg_mesh, color=color)
                 else:
                     seg_mesh = pv.Tube(
                         pointa=(p.x * factor, p.z * factor, -1 * p.y * factor),
@@ -63,36 +65,42 @@ def add_neuroml_model(
                         n_sides=15,
                     )
 
-                cell_meshes.append(seg_mesh)
+                seg_meshes.append(seg_mesh)
 
-            while pos_pop:
-                cell_index, pos = pos_pop.popitem()
-                pp = [pos[0] * factor, pos[2] * factor, -1 * pos[1] * factor]
-                print("Plotting %s(%i) at %s, %s" % (cell.id, cell_index, pos, pp))
-                cell_mesh = cell_meshes.copy()
-                for i in range(len(cell_mesh)):
-                    cell_mesh[i].translate(pp, inplace=True)
-                plotter.add_mesh(cell_mesh, color=color, smooth_shading=True)
+            # one template mesh per cell type, stamped at every instance position
+            # via a glyph filter instead of copying/translating it per instance
+            cell_mesh = (
+                seg_meshes[0].merge(seg_meshes[1:])
+                if len(seg_meshes) > 1
+                else seg_meshes[0]
+            )
+            glyphs = point_cloud.glyph(geom=cell_mesh, scale=False, orient=False)
+            plotter.add_mesh(glyphs, color=color, smooth_shading=True)
 
         else:
-            while pos_pop:
-                cell_index, pos = pos_pop.popitem()
-                pp = [pos[0] * factor, pos[2] * factor, -1 * pos[1] * factor]
-                print(
-                    "Plotting only sphere (radius: %s) %s(%i) at %s, %s"
-                    % (radius, cell.id, cell_index, pos, pp)
-                )
-                sphere = pv.Sphere(center=(pp[0], pp[1], pp[2]), radius=radius)
-
-                plotter.add_mesh(sphere, color=color)
+            sphere = pv.Sphere(radius=radius)
+            glyphs = point_cloud.glyph(geom=sphere, scale=False, orient=False)
+            plotter.add_mesh(glyphs, color=color)
 
 
 if __name__ == "__main__":
     plotter = pv.Plotter()
 
-    add_neuroml_model(plotter, "NeuroML2/c302_D_Full.net.nml", somas_only=False)
+    c302_nml = "NeuroML2/c302_D_Full.net.nml"
+
+    show_gui = True
+    if "-nogui" in sys.argv:
+        show_gui = False
+        sys.argv.remove("-nogui")
+
+    if len(sys.argv) > 1 and sys.argv[1].endswith(".nml"):
+        nml_file = sys.argv[1]
+    else:
+        nml_file = c302_nml
+
+    add_neuroml_model(plotter, nml_file, somas_only=False)
     plotter.set_background("white")
     plotter.add_axes()
 
-    if "-nogui" not in sys.argv:
+    if show_gui:
         plotter.show()
