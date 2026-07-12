3875f72c44735b0638baf3e5560b58ea17890191
Padraig Gleeson <p.gleeson@gmail.com>
2026-07-12 21:41:11 +0800

    Hide footprint of sibernetic

--- load.py
+++ load.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
 
     spacing = 50
 
-    add_sibernetic_model(plotter, swap_y_z=True, offset3d=(spacing, -50, -100))
+    add_sibernetic_model(
+        plotter, swap_y_z=True, offset3d=(spacing, -50, -100), show_footprint=False
+    )
     add_neuroml_model(
         plotter,
         "NeuroML2/c302_D_Full.net.nml",
